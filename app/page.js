2bd340ec83765bf7475a6968470665c7cc6a8931
Chandrashekar <chandrashekar31.v@gmail.com>
2024-05-01 17:30:17 +0530

    changed responsiveness

--- app/page.js
+++ app/page.js
@@ -11,16 +11,16 @@ export default function Home(props) {
   return (
     <>
       <div className="flex flex-col items-center min-h-screen max-w-screen bg-slate-500">
-        <LazyLoad width={600} height={400} threshold={0.60}>
+        <LazyLoad threshold={0.60}>
           <SkeletonImage url="https://www.shutterstock.com/image-photo/vidhan-soudha-bangalore-_-image-600nw-1433353757.jpg" alt="bangalore-place" />
         </LazyLoad>
-        <LazyLoad width={600} height={400} threshold={0.60}>
+        <LazyLoad threshold={0.60}>
           <SkeletonImage url="https://www.shutterstock.com/image-photo/vidhan-soudha-bangalore-_-image-600nw-1433353757.jpg" alt="bangalore-place" />
         </LazyLoad>
-        <LazyLoad width={600} height={400} threshold={0.60}>
+        <LazyLoad threshold={0.60}>
           <SkeletonImage url="https://www.shutterstock.com/image-photo/vidhan-soudha-bangalore-_-image-600nw-1433353757.jpg" alt="bangalore-place" />
         </LazyLoad>
-        <LazyLoad width={600} height={400} threshold={0.60}>
+        <LazyLoad threshold={0.60}>
           <SkeletonImage url="https://www.shutterstock.com/image-photo/vidhan-soudha-bangalore-_-image-600nw-1433353757.jpg" alt="bangalore-place" />
         </LazyLoad>
 
