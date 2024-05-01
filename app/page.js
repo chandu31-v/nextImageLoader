d7f64569620719346d116f6c02baa9044504caf8
Chandrashekar <chandrashekar31.v@gmail.com>
2024-05-01 15:37:52 +0530

    changed from pages to app based

--- app/page.js
+++ app/page.js
@@ -1,8 +1,13 @@
+"use client"
+
 import React from "react";
 import LazyLoad from "react-lazy-load";
 import SkeletonImage from "@/components/skeletonImage";
 
-export default function Home() {
+export default function Home(props) {
+
+  //console.log(props.url)
+
   return (
     <>
       <div className="flex flex-col items-center min-h-screen max-w-screen bg-slate-500">
@@ -24,21 +29,31 @@ export default function Home() {
   );
 }
 
-/*
+
 // staticProps can be used to pre-generate page on server and present on server
 // serversideprops can be used to generate page on server on demand
 
-// export async function getStaticProps(context) {
-
-//   const data = images
-
+// export async function getStaticProps(Imageprops) {
 //   return (
 //       {
 //           props: {
-//               data: data.events
+//               data: Imageprops
 //           }
 //       }
 //   )
 
 // }
-*/
+
+// export async function getStaticPaths() {
+
+//   //fetch data from getImagesUrl api
+//   const data = await fetch("./api/getImagesUrl")
+  
+  
+//   return (
+//       {
+//           paths: data,
+//           fallback: true
+//       }
+//   )
+// }
